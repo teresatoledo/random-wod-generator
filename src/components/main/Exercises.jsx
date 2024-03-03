1372199a672d009a24de7fafc7554915ca1e80fe
Teresa Toledo <laratoledoteresa@hotmail.com>
2024-03-03 17:09:10 +0100

    Fix boxes

--- src/components/main/Exercises.jsx
+++ src/components/main/Exercises.jsx
@@ -6,254 +6,280 @@ function Exercises() {
 	return (
 		<>
 			<Header />
-			<div className="exercise">
-				<Link to="/main/">
-					<button className="exercise__button">Back</button>
-				</Link>
-				<h2 className="exercise__title">
-					Quick video for all exercises
-				</h2>
-				<h4 className="exercise__subtitle">Weighlifting</h4>
-				<ul className="exercise__ul">
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=4v02go3uTBc&ab_channel=CrossFitSANTIAGO"
-							target="_blank"
-						>
-							Squat snatch
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=TL8SMp7RdXQ"
-							target="_blank"
-						>
-							Power snatch
-						</a>
-					</li>
-					<li>
-						<a
-							rel="noopener noreferrer"
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=Ty14ogq_Vok"
-							target="_blank"
-						>
-							Squat clean
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=KwYJTpQ_x5A"
-							target="_blank"
-						>
-							Power clean
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=PjY1rH4_MOA"
-							target="_blank"
-						>
-							Clean and jerk
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=VrHNJXoSyXw"
-							target="_blank"
-						>
-							Push jerk
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=GUDkOtraHHY"
-							target="_blank"
-						>
-							Split jerk
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=UUcXwF4shCg"
-							target="_blank"
-						>
-							Shoulder to overhead (STOH)
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=5nyasj1quW0"
-							target="_blank"
-						>
-							Ground to overhead (GTOH)
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=3mlhF3dptAo"
-							target="_blank"
-						>
-							Dumbell snatch
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=CUaxieWW0tw"
-							target="_blank"
-						>
-							Dumbell clean
-						</a>
-					</li>
-				</ul>
-				<h4 className="guide__subtitle">Gymnastics</h4>
-				<ul className="exercise__ul">
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=lzRo-4pq_AY"
-							target="_blank"
-						>
-							Pull up
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=p9Stan68FYM"
-							target="_blank"
-						>
-							Chest to bar
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=OCg3UXgzftc"
-							target="_blank"
-						>
-							Bar muscle up
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=_03pCKOv4l4"
-							target="_blank"
-						>
-							Toes to bar
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=faJDYEZmueM"
-							target="_blank"
-						>
-							Pull over
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=InRvHNUOlSs"
-							target="_blank"
-						>
-							HSPU
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=0wDEO6shVjc"
-							target="_blank"
-						>
-							Strict HSPU
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=I5p2VVDupq8"
-							target="_blank"
-						>
-							HSW
-						</a>
-					</li>
-				</ul>
-				<h4 className="guide__subtitle">Endurance</h4>
-				<ul className="exercise__ul">
-					<li>
-						<a className="exercise__desc" href="#">
-							Calories in row/assault bike/echo bike/ski
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=fpUD0mcFp_0"
-							target="_blank"
-						>
-							Wall ball
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=L219ltL15zk"
-							target="_blank"
-						>
-							Thursters
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=hCuXYrTOMxI"
-							target="_blank"
-						>
-							Single Under (SU)
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=-tF3hUsPZAI"
-							target="_blank"
-						>
-							Double Under (DU)
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=81wWS0rAaDk"
-							target="_blank"
-						>
-							Devil press
-						</a>
-					</li>
-					<li>
-						<a
-							className="exercise__desc"
-							href="https://www.youtube.com/watch?v=auBLPXO8Fww"
-							target="_blank"
-						>
-							Burpees
-						</a>
-					</li>
-				</ul>
-			</div>
+			<article className="article">
+				<div className="exercise">
+					<Link to="/main/">
+						<button className="exercise__button">Back</button>
+					</Link>
+					<h2 className="exercise__title">
+						Quick video for all exercises
+					</h2>
+					<h4 className="exercise__subtitle">Weighlifting</h4>
+					<ul className="exercise__ul">
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=4v02go3uTBc&ab_channel=CrossFitSANTIAGO"
+								target="_blank"
+							>
+								Squat snatch
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=TL8SMp7RdXQ"
+								target="_blank"
+							>
+								Power snatch
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=Ty14ogq_Vok"
+								target="_blank"
+							>
+								Squat clean
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=KwYJTpQ_x5A"
+								target="_blank"
+							>
+								Power clean
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=PjY1rH4_MOA"
+								target="_blank"
+							>
+								Clean and jerk
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=VrHNJXoSyXw"
+								target="_blank"
+							>
+								Push jerk
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=GUDkOtraHHY"
+								target="_blank"
+							>
+								Split jerk
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=UUcXwF4shCg"
+								target="_blank"
+							>
+								Shoulder to overhead (STOH)
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=5nyasj1quW0"
+								target="_blank"
+							>
+								Ground to overhead (GTOH)
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=3mlhF3dptAo"
+								target="_blank"
+							>
+								Dumbell snatch
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=CUaxieWW0tw"
+								target="_blank"
+							>
+								Dumbell clean
+							</a>
+						</li>
+					</ul>
+					<h4 className="guide__subtitle">Gymnastics</h4>
+					<ul className="exercise__ul">
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=lzRo-4pq_AY"
+								target="_blank"
+							>
+								Pull up
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=p9Stan68FYM"
+								target="_blank"
+							>
+								Chest to bar
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=OCg3UXgzftc"
+								target="_blank"
+							>
+								Bar muscle up
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=_03pCKOv4l4"
+								target="_blank"
+							>
+								Toes to bar
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=faJDYEZmueM"
+								target="_blank"
+							>
+								Pull over
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=InRvHNUOlSs"
+								target="_blank"
+							>
+								HSPU
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=0wDEO6shVjc"
+								target="_blank"
+							>
+								Strict HSPU
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=I5p2VVDupq8"
+								target="_blank"
+							>
+								HSW
+							</a>
+						</li>
+					</ul>
+					<h4 className="guide__subtitle">Endurance</h4>
+					<ul className="exercise__ul">
+						<li>
+							<a className="exercise__desc" href="#">
+								Calories in row/assault bike/echo bike/ski
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=fpUD0mcFp_0"
+								target="_blank"
+							>
+								Wall ball
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=L219ltL15zk"
+								target="_blank"
+							>
+								Thursters
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=hCuXYrTOMxI"
+								target="_blank"
+							>
+								Single Under (SU)
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=-tF3hUsPZAI"
+								target="_blank"
+							>
+								Double Under (DU)
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=81wWS0rAaDk"
+								target="_blank"
+							>
+								Devil press
+							</a>
+						</li>
+						<li>
+							<a
+								rel="noopener noreferrer"
+								className="exercise__desc"
+								href="https://www.youtube.com/watch?v=auBLPXO8Fww"
+								target="_blank"
+							>
+								Burpees
+							</a>
+						</li>
+					</ul>
+				</div>
+			</article>
 		</>
 	);
 }
--- src/components/main/Guide.jsx
+++ src/components/main/Guide.jsx
@@ -5,62 +5,69 @@ function Guide() {
 	return (
 		<>
 			<Header />
-			<div className="guide">
-				<Link to="/main/">
-					<button className="guide__button">Back</button>
-				</Link>
-				<h2 className="guide__title">Quick how to use guide</h2>
-				<h4 className="guide__subtitle">Type of wod</h4>
-				<p className="guide__text">
-					You can choose between the most common type of wods:
-				</p>
-				<p className="guide__text">
-					<span className="guide__text--title">AMRAP:</span> As many
-					reps as possible. You will be given a time frame and you
-					will have to repeat the given exercises as long as time is
-					runnig.
-				</p>
-				<p className="guide__text">
-					<span className="guide__text--title">EMOM:</span> Every
-					minute on the minute. You will have to do the given
-					exercise/reps in a minute. The remaning time of that time
-					once you finish will be rest.
-				</p>
-				<p className="guide__text">
-					<span className="guide__text--title">For time:</span> You
-					should do the rounds and exercises indicated just once.
-				</p>
-				<p className="guide__text">
-					<span className="guide__text--title">For quality:</span> In
-					this kind of workout, the number of reps or time are not the
-					main focus. In for quality wods, the main focus should be
-					the technique and the quality of the movements indicated.
-				</p>
-				<h4 className="guide__subtitle">Main focus</h4>
-				<p className="guide__text">
-					<span className="guide__text--title">Weightligting:</span>{' '}
-					The majority of exercises will be weightligting focused:
-					snatch, clean, jerk...
-				</p>
-				<p className="guide__text">
-					<span className="guide__text--title">Gymnastics:</span> The
-					majority of exercises will be gymnastics focused: pull-ups,
-					chest to bar, toes to bar, bar muscle ups. HSPU...
-				</p>
-				<p className="guide__text">
-					<span className="guide__text--title">Endurance:</span> The
-					majority of exercises will be endurance focused: cal, run,
-					wallball, burpees...
-				</p>
-				<p className="guide__text">
-					<span className="guide__text--title">All in one:</span> Wod
-					will be a mix of all the above.
-				</p>
-				<h4 className="guide__subtitle">Duration</h4>
-				<p className="guide__text">
-					Wod duration will be between range selected.
-				</p>
-			</div>
+			<article className="article">
+				<div className="guide">
+					<Link to="/main/">
+						<button className="guide__button">Back</button>
+					</Link>
+					<h2 className="guide__title">Quick how to use guide</h2>
+					<h4 className="guide__subtitle">Type of wod</h4>
+					<p className="guide__text">
+						You can choose between the most common type of wods:
+					</p>
+					<p className="guide__text">
+						<span className="guide__text--title">AMRAP:</span> As
+						many reps as possible. You will be given a time frame
+						and you will have to repeat the given exercises as long
+						as time is runnig.
+					</p>
+					<p className="guide__text">
+						<span className="guide__text--title">EMOM:</span> Every
+						minute on the minute. You will have to do the given
+						exercise/reps in a minute. The remaning time of that
+						time once you finish will be rest.
+					</p>
+					<p className="guide__text">
+						<span className="guide__text--title">For time:</span>{' '}
+						You should do the rounds and exercises indicated just
+						once.
+					</p>
+					<p className="guide__text">
+						<span className="guide__text--title">For quality:</span>{' '}
+						In this kind of workout, the number of reps or time are
+						not the main focus. In for quality wods, the main focus
+						should be the technique and the quality of the movements
+						indicated.
+					</p>
+					<h4 className="guide__subtitle">Main focus</h4>
+					<p className="guide__text">
+						<span className="guide__text--title">
+							Weightligting:
+						</span>{' '}
+						The majority of exercises will be weightligting focused:
+						snatch, clean, jerk...
+					</p>
+					<p className="guide__text">
+						<span className="guide__text--title">Gymnastics:</span>{' '}
+						The majority of exercises will be gymnastics focused:
+						pull-ups, chest to bar, toes to bar, bar muscle ups.
+						HSPU...
+					</p>
+					<p className="guide__text">
+						<span className="guide__text--title">Endurance:</span>{' '}
+						The majority of exercises will be endurance focused:
+						cal, run, wallball, burpees...
+					</p>
+					<p className="guide__text">
+						<span className="guide__text--title">All in one:</span>{' '}
+						Wod will be a mix of all the above.
+					</p>
+					<h4 className="guide__subtitle">Duration</h4>
+					<p className="guide__text">
+						Wod duration will be between range selected.
+					</p>
+				</div>
+			</article>
 		</>
 	);
 }
